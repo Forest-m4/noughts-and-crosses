2831cf43a3deda950d001e5a86e8a2d38ec51e39
Forest-m4 <makarova.dariya@icloud.com>
2025-10-21 21:42:54 +0300

    feat: добавлен метод reset() для перезапуска игры и демонстрация

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import game.TicTacToe;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         TicTacToe game = new TicTacToe(3);
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Демонстрация restart:");
         game.printBoard();
-        System.out.println("Нач версия: поле создано. Добавлю фичи в отдельных ветках.");
+
+        // делаем пару ходов вручную в demo
+        game.placeMark(0,0,'X');
+        game.placeMark(1,1,'O');
+        System.out.println("После нескольких ходов:");
+        game.printBoard();
+
+        System.out.println("Делаем reset...");
+        game.reset();
+        game.printBoard();
     }
 }
---
+++ src/game/TicTacToe.java
@@ -0,0 +1,43 @@
+package game;
+public class TicTacToe {
+    private char[][] board;
+    private int size;
+
+    public TicTacToe(int size) {
+        this.size = size;
+        board = new char[size][size];
+        initBoard();
+    }
+
+    private void initBoard() {
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                board[i][j] = ' ';
+    }
+
+    public boolean placeMark(int row, int col, char mark) {
+        if (row < 0 || row >= size || col < 0 || col >= size) return false;
+        if (board[row][col] != ' ') return false;
+        board[row][col] = mark;
+        return true;
+    }
+    public void reset() {
+        initBoard(); // переинициализируем поле
+    }
+    public void printBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" " + board[i][j]);
+                if (j < size - 1) System.out.print(" |");
+            }
+            System.out.println();
+            if (i < size - 1) {
+                for (int k = 0; k < size; k++) {
+                    System.out.print("---");
+                    if (k < size - 1) System.out.print("+");
+                }
+                System.out.println();
+            }
+        }
+    }
+}

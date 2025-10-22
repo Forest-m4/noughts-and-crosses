50370d2e2279249474b1c747b24aeaf3500b80c3
Forest-m4 <makarova.dariya@icloud.com>
2025-10-22 20:49:40 +0300

    add_new_feature: добавлен файл NewFeature.java с базовым функционалом

---
+++ src/NewFeature.java
@@ -0,0 +1,11 @@
+public class NewFeature {
+    public static void showFeature() {
+        System.out.println("=== Game Feature ===");
+        System.out.println("Теперь игра может показывать базовую информацию о сеансе!");
+        System.out.println("====================");
+    }
+
+    public static void main(String[] args) {
+        showFeature();
+    }
+}
